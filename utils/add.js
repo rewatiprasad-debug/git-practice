2f2ea65f2dca8061cd0d2749abd89efc4f96880e
raman <rewati.prasad@ethara.ai>
2026-04-06 16:09:55 +0530

    added comment for creating function

---
+++ utils/add.js
@@ -0,0 +1,16 @@
+// create division function
+
+
+
+
+//create addition function
+export const add=(a,b)=>{
+    return a+b;
+}
+
+// create subtraction function
+
+
+
+
+// create multiplication function
